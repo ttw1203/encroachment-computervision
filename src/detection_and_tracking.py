f1449870c73213ebfa1719b26ab80dde27bbc7f2
BAK057 <bfp141@gmail.com>
2025-06-09 00:09:31 +0600

    Set smoothening to 0, min speed threshold to 0.1.

--- src/detection_and_tracking.py
+++ src/detection_and_tracking.py
@@ -22,7 +22,7 @@ class DetectionTracker:
         self.video_fps = video_fps
 
         # Initialize detection smoother
-        self.smoother = sv.DetectionsSmoother(length=0)
+        self.smoother = sv.DetectionsSmoother(0)
 
         # Initialize detection model
         if detector_model == "rf_detr":
