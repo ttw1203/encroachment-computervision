c9713e434688adcf730471080d44316821e162f8
bakF057 <bfp141@gmail.com>
2025-06-21 14:23:55 +0600

    Update TTC thresholds in configuration files for improved monitoring accuracy

--- src/config.py
+++ src/config.py
@@ -125,7 +125,7 @@ class Config:
         self.MOVE_THRESH_METRES = 2
         self.DEFAULT_NUM_FUTURE_PREDICTIONS = 10
         self.DEFAULT_FUTURE_PREDICTION_INTERVAL = 0.1
-        self.DEFAULT_TTC_THRESHOLD = 1.0
+        self.DEFAULT_TTC_THRESHOLD = 5.0
         self.MAX_AGE_SECONDS = 1.5
         self.COLLISION_DISTANCE = 2.0
 
